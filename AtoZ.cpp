216cd8f2daf0ef0167f521829e050e512618341b
curiouschahar <shantanuchahar01@gmail.com>
2025-10-31 19:50:54 +0530

    Updated AtoZ.cpp with new changes

--- AtoZ.cpp
+++ AtoZ.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     cout<<"Enter a number: ";
     char name;
-    for(name='a';name<='z';name++){
+    for(name='a';name<='y';name++){
         cout<<name<<endl;
     }
     return 0;
